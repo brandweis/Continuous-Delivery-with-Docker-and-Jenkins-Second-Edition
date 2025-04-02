f292f3922e89375cd48566e049e52e73e0c985ee
Brandon Weishaar <Brandonweishaar02@gmail.com>
2025-04-01 21:03:22 -0400

    added /div endpoint to calculator and updated image

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,22 +3,16 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-/**
- * This is the calculator class.
- * It Provides basic functionality.
- */
+/** Calculator logic */
 @Service
 public class Calculator {
-        final static int umlNUMBER1 = 3;
-	
-	/**
-	 * adds two numbers together.
-	 * @param a first number
-	 * @param b second number
-	 * @return the sum of and a and b
-	 */
 	@Cacheable("sum")
 	public int sum(int a, int b) {
 		return a + b;
 	}
+
+        @Cacheable("div")
+        public int div(int a, int b) {
+                return a / b;
+        }
 }
